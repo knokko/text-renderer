da291090bb8263fea3ccf367041bba30e8af6097
knokko <knokogator@hotmail.com>
2024-08-07 19:46:12 +0200

    Use item_offset and item_length in hb_buffer_add_utf16

--- core/src/main/java/com/github/knokko/text/placement/TextSplitter.java
+++ core/src/main/java/com/github/knokko/text/placement/TextSplitter.java
@@ -3,6 +3,7 @@ package com.github.knokko.text.placement;
 import org.lwjgl.system.MemoryStack;
 import org.lwjgl.util.harfbuzz.hb_glyph_info_t;
 
+import java.nio.ByteBuffer;
 import java.text.Bidi;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,11 +29,15 @@ class TextSplitter {
 		wasBaseLeftToRight = bidi.baseIsLeftToRight();
 		List<TextRun> runs = new ArrayList<>();
 
+		ByteBuffer originalStringBuffer = stack.UTF16(originalText);
+
 		for (int bidiRun = 0; bidiRun < bidi.getRunCount(); bidiRun++) {
 			String bidiRunString = originalText.substring(bidi.getRunStart(bidiRun), bidi.getRunLimit(bidiRun));
 			if (bidiRunString.isEmpty()) continue;
 
-			List<TextRun> newRuns = splitForRightFace(bidiRunString, bidi.getRunStart(bidiRun), 0, stack);
+			List<TextRun> newRuns = splitForRightFace(
+					originalText, originalStringBuffer, bidi.getRunStart(bidiRun), bidi.getRunLimit(bidiRun), 0
+			);
 			if (bidi.getRunLevel(bidiRun) % 2 == 1) Collections.reverse(newRuns);
 
 			if (wasBaseLeftToRight) runs.addAll(merge(newRuns));
@@ -61,52 +66,58 @@ class TextSplitter {
 		return merged;
 	}
 
-	private List<TextRun> splitForRightFace(String largeString, int offset, int faceIndex, MemoryStack stack) {
-		if (largeString.isEmpty()) return Collections.emptyList();
+	private List<TextRun> splitForRightFace(
+			String originalString, ByteBuffer originalStringBuffer,
+			int offset, int limit, int faceIndex
+	) {
+		if (limit <= offset) return Collections.emptyList();
 
 		hb_buffer_reset(hbBuffer);
-		// TODO Maybe use item_offset and item_length instead of substring
-		hb_buffer_add_utf16(hbBuffer, stack.UTF16(largeString), 0, -1);
+		hb_buffer_add_utf16(hbBuffer, originalStringBuffer, offset, limit - offset);
 		hb_buffer_guess_segment_properties(hbBuffer);
 		hb_buffer_set_cluster_level(hbBuffer, HB_BUFFER_CLUSTER_LEVEL_MONOTONE_CHARACTERS);
 		hb_shape(hbFonts[faceIndex], hbBuffer, null);
 		var glyphInfo = hb_buffer_get_glyph_infos(hbBuffer);
-		List<Substring> substrings = computeSubstrings(largeString, glyphInfo, faceIndex);
+		List<Substring> substrings = computeSubstrings(originalString, offset, limit, glyphInfo, faceIndex);
 
 		List<TextRun> runs = new ArrayList<>(substrings.size());
 		for (Substring substring : substrings) {
-			int totalOffset = offset + substring.startIndex();
-			String smallString = largeString.substring(substring.startIndex(), substring.limit());
+			String smallString = originalString.substring(substring.startIndex(), substring.limit());
 
-			if (substring.succeeded()) runs.add(new TextRun(smallString, faceIndex, totalOffset));
+			if (substring.succeeded()) runs.add(new TextRun(smallString, faceIndex, substring.startIndex()));
 			else if (faceIndex + 1 < hbFonts.length) runs.addAll(splitForRightFace(
-					smallString, totalOffset, faceIndex + 1, stack
+					originalString, originalStringBuffer, substring.startIndex(), substring.limit(), faceIndex + 1
 			));
-			else runs.add(new TextRun(smallString, 0, totalOffset));
+			else runs.add(new TextRun(smallString, 0, substring.startIndex()));
 		}
 		return runs;
 	}
 
-	private List<Substring> computeSubstrings(String currentString, hb_glyph_info_t.Buffer glyphs, int faceIndex) {
+	private List<Substring> computeSubstrings(
+			String originalString, int offset, int limit,
+			hb_glyph_info_t.Buffer glyphs, int faceIndex
+	) {
 		if (glyphs == null) throw new RuntimeException("Glyph info is null");
 
-		boolean[] supported = new boolean[currentString.length()];
+		boolean[] supported = new boolean[limit - offset];
 		if (supported.length == 0) return Collections.emptyList();
 		Arrays.fill(supported, true);
 
 		for (int index = 0; index < glyphs.limit(); index++) {
 			var glyph = glyphs.get(index);
+			int cluster = glyph.cluster() - offset;
+
 			// Not sure why harfbuzz does this, but let's just ignore it
-			if (glyph.cluster() >= supported.length) continue;
+			if (cluster >= supported.length) continue;
 
 			if (glyph.codepoint() == 0) {
-				supported[glyph.cluster()] = false;
-				int codepoint = currentString.codePointAt(glyph.cluster());
+				supported[cluster] = false;
+				int codepoint = originalString.codePointAt(offset + cluster);
 				if (Character.isSupplementaryCodePoint(codepoint)) {
-					supported[glyph.cluster() + 1] = false;
+					supported[cluster + 1] = false;
 				}
 				if ((glyph.mask() & HB_GLYPH_FLAG_UNSAFE_TO_BREAK) != 0) {
-					supported[glyph.cluster() - 1] = false;
+					supported[cluster - 1] = false;
 				}
 			}
 		}
@@ -118,12 +129,12 @@ class TextSplitter {
 		for (int index = 0; index < supported.length; index++) {
 			if (supported[index] == wasSupported) continue;
 
-			substrings.add(new Substring(startIndex, index, faceIndex, wasSupported));
+			substrings.add(new Substring(startIndex + offset, index + offset, faceIndex, wasSupported));
 			startIndex = index;
 			wasSupported = !wasSupported;
 		}
 
-		substrings.add(new Substring(startIndex, supported.length, faceIndex, wasSupported));
+		substrings.add(new Substring(startIndex + offset, supported.length + offset, faceIndex, wasSupported));
 		return substrings;
 	}
 
