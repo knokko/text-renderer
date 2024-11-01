f0f7a9cbc1b76ab55ae68a3f630e64cb38c934b9
knokko <knokogator@hotmail.com>
2024-11-01 19:22:05 +0100

    Add doc comments to UnicodeFonts

--- unicode-fonts/src/main/java/com/github/knokko/text/font/UnicodeFonts.java
+++ unicode-fonts/src/main/java/com/github/knokko/text/font/UnicodeFonts.java
@@ -1,7 +1,15 @@
 package com.github.knokko.text.font;
 
+/**
+ * This class (only) has {@link #SOURCE}
+ */
 public class UnicodeFonts {
 
+	/**
+	 * A {@link FontSource} that combines 16 fonts that together cover most unicode characters, all of which are free
+	 * to use for both personal and commercial projects. This font source should be an excellent fallback font
+	 * (or a simple font for testing).
+	 */
 	public static final FontSource SOURCE = new ClasspathFontsSource(
 			"fonts/unicode-freeserif.ttf",
 			"fonts/unicode-quivira.ttf",
